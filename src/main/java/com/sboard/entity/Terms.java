6908b1cec1815a42ea5643e9b3926bbbe80e883b
ekkang2 <rkddmsrud27@gmail.com>
2024-09-20 16:12:16 +0900

    commit 0.0.1

---
+++ src/main/java/com/sboard/entity/Terms.java
@@ -0,0 +1,35 @@
+package com.sboard.entity;
+
+
+import com.sboard.dto.TermsDTO;
+import com.sboard.dto.UserDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "terms")
+public class Terms {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int seq;
+
+    @Column(name = "terms", columnDefinition = "TEXT")
+    private String terms;
+    @Column(name = "privacy", columnDefinition = "TEXT")
+    private String privacy;
+
+    public TermsDTO toDTO(){
+        return TermsDTO.builder()
+                .seq(seq)
+                .terms(terms)
+                .privacy(privacy)
+                .build();
+    }
+}
